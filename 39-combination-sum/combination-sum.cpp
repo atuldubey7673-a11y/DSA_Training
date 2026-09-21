8c99f1465657eb60814d2599ac32efb3962da99e
Atul Dubey <atuldubey7673@gmail.com>
2026-09-22 00:03:36 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (57.28%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    vector<int> temp;
 
-    void solve(int index, vector<int>& candidates, int target) {
-
-        // Target achieved
-        if (target == 0) {
-            ans.push_back(temp);
-            return;
-        }
-
-        // Out of bounds
-        if (index == candidates.size()) {
-            return;
-        }
-
-        // Take current element
-        if (candidates[index] <= target) {
-            temp.push_back(candidates[index]);
-            solve(index, candidates, target - candidates[index]);
-            temp.pop_back();   // Backtrack
-        }
-
-        // Skip current element
-        solve(index + 1, candidates, target);
+void solve(vector<int>&candidates,int target,int index,vector<int>&current,vector<vector<int>>&ans){
+    if(target==0){
+        ans.push_back(current);
+        return;
     }
-
+    if(index>=candidates.size() || target<0){
+        return ;
+    }
+    current.push_back(candidates[index]);
+    solve(candidates,target-candidates[index],index,current,ans);
+    current.pop_back();
+    solve(candidates,target,index+1,current,ans);
+}
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        solve(0, candidates, target);
-        return ans;
+       vector<int>current;
+       vector<vector<int>>ans ;
+       solve(candidates,target,0,current,ans);
+       return ans;
     }
 };
